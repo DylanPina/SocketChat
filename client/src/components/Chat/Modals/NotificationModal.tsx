eebb69e8344a262f1e1b18c19a1f933c37e9d42a
Dylan Pina <dillon_idgafos@yahoo.com>
2022-05-23 21:23:59 -0400

    Finished notifications UI for OneToOne notifications

--- client/src/components/Chat/Modals/NotificationModal.tsx
+++ client/src/components/Chat/Modals/NotificationModal.tsx
@@ -5,6 +5,8 @@ import { setSelectedChat } from "../../../redux/chats/chats.slice";
 import { getSender } from "../../../config/ChatLogic";
 import { Message } from "../../../types/message.types";
 
+import { IconContext } from "react-icons";
+import { HiUserGroup } from "react-icons/hi";
 import styles from "../../../styles/ChatPage/Modals/NotificationModal.module.css";
 
 const NotificationModal = () => {
@@ -25,20 +27,31 @@ const NotificationModal = () => {
 	return (
 		<div className={styles.noti_modal}>
 			<div className={styles.noti_header_container}>
-				<h1 className={styles.noti_header}>{`Notifications ${notifications.length}`}</h1>
+				<h1 className={styles.noti_header}>{`Notifications: ${notifications.length}`}</h1>
 			</div>
 			<div className={styles.noti_container}>
 				{!notifications.length ? (
 					<h1 className={styles.no_noti}>No new notifications</h1>
 				) : (
 					notifications.map((noti: Message) => (
-						<div className={styles.single_noti_container} key={noti._id} onClick={() => handleNotiClick(noti)}>
+						<>
 							{noti.chat.isGroupChat ? (
-								<h1 className={styles.single_noti}>{`New message in ${noti.chat.chatName}`}</h1>
+								<div className={styles.single_noti_container} key={noti._id} onClick={() => handleNotiClick(noti)}>
+									<IconContext.Provider value={{ className: styles.groupchat_icon }}>
+										<HiUserGroup />
+									</IconContext.Provider>
+									<h1 className={styles.single_noti}>{`New message in ${noti.chat.chatName}`}</h1>
+								</div>
 							) : (
-								<h1 className={styles.single_noti}>{`New message from ${getSender(user, noti.chat.users).username}`}</h1>
+								<div className={styles.single_noti_container} key={noti._id} onClick={() => handleNotiClick(noti)}>
+									<img src={`${noti.sender.profilePic}`} alt={`${noti.sender.username}`} className={styles.sender_profile_pic} />
+									<div className={styles.single_noti_content_container}>
+										<h3 className={styles.single_noti_sender_name}>{`${getSender(user, noti.chat.users).username}`}</h3>
+										<p className={styles.single_noti_msg_content}>{noti.content}</p>
+									</div>
+								</div>
 							)}
-						</div>
+						</>
 					))
 				)}
 			</div>
