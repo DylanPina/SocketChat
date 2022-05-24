e9bfdba68d40d9a64e2b081bfaf4580c102112d0
Dylan Pina <dillon_idgafos@yahoo.com>
2022-05-23 22:24:10 -0400

    Added latest message to MyChats component

--- client/src/components/Chat/MyChats.tsx
+++ client/src/components/Chat/MyChats.tsx
@@ -84,7 +84,15 @@ const MyChats = () => {
 							) : (
 								<>
 									<img className={styles.chat_pic} src={chat.users[0].profilePic || ""} alt={chat.users[0].username} />
-									<h1 className={styles.chat_name}>{getSender(currentUser, chat.users).username}</h1>
+									<div className={styles.chat_info_container}>
+										<h1 className={styles.chat_sender_username}>{getSender(currentUser, chat.users).username}</h1>
+										{chat.latestMessage && (
+											<p className={styles.chat_latest_msg_sender}>
+												{`${chat.latestMessage.sender.username}: `}
+												<span className={styles.chat_latest_msg}>{chat.latestMessage.content}</span>
+											</p>
+										)}
+									</div>
 								</>
 							)}
 						</div>
