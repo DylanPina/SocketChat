2113f579971e1b3ab8fd317c2f62aa86edf89753
Dylan Pina <dillon_idgafos@yahoo.com>
2022-07-02 22:32:27 -0400

    Updated dirname in server.ts

--- server/server.ts
+++ server/server.ts
@@ -37,7 +37,7 @@ const __dirname1 = path.resolve();
 console.log(path.join(__dirname1, "../client/build"));
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname1, "../client/build")));
-	app.get("*", (req, res) => res.sendFile(path.resolve(__dirname1, "../client", "build", "index.html")));
+	app.get("*", (req, res) => res.sendFile(path.resolve(__dirname1, "../client/build", "index.html")));
 } else {
 	app.get("/", (req, res) => res.send("API is Running"));
 }
